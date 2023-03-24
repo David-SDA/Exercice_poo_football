cad63f8b204aaca62a4e63c8fb3951b48fd50ed3
David Sousa de Araujo <davidaraujo2001@gmail.com>
2023-03-24 10:08:35 +0100

    Ajout Getter/Setter, construct/toString, Pays

--- Equipe.class.php
+++ Equipe.class.php
@@ -4,5 +4,49 @@
         private Pays $_pays;
         private DateTime $_dateCreation;
         private array $_contracts = [];
+
+        /* Méthode __construct de la classe */
+        public function __construct(string $nom, Pays $pays, string $dateCreation){
+            $this->_nom = $nom;
+            $this->_pays = $pays;
+            $this->_dateCreation = new DateTime($dateCreation);
+        }
+
+        /* Getter et Setter pour le nom de l'équipe */
+        public function getNom() : string{
+            return $this->_nom;
+        }
+        public function setNom(string $nom){
+            $this->_nom = $nom;
+        }
+
+        /* Getter et Setter pour le pays de l'équipe */
+        public function getPays() : Pays{
+            return $this->_pays;
+        }
+        public function setPays(Pays $pays){
+            $this->_pays = $pays;
+        }
+
+        /* Getter et Setter pour la date de création de l'équipe */
+        public function getDateCreation() : DateTime{
+            return $this->_dateCreation;
+        }
+        public function setDateCreation(string $dateCreation){
+            $this->_dateCreation = new DateTime($dateCreation);
+        }
+
+        /* Getter et Setter pour les contrats de l'équipe */
+        public function getContracts() : array{
+            return $this->_contracts;
+        }
+        public function setContracts(Contract $contract){
+            array_push($this->_contracts, $contract);
+        }
+
+        /* Méthode __toString de la classe */
+        public function __toString(){
+            return $this->_nom;
+        }
     }
 ?>
